d5942e23fe755c64ef088cb5a5e8e596bd0c37c3
docbronovan <brockdonovan@gmail.com>
2013-08-08 19:54:54 +0000

    2nd try read buffer

--- web.js
+++ web.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-fs.readFileSync('index.html' {
+fs.readFileSync('index.html') {
   response.send(Buffer.toString());
-});
+};
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
