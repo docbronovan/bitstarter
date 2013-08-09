85df3ef9b4ba7105094c1e591e8d78246a123b76
docbronovan <brockdonovan@gmail.com>
2013-08-09 14:36:41 +0000

    4th

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var k = fs.readFileSync('index.html');
-console.log(response.send(Buffer.toString(k)));
+if (index.html) {
+       rest.get(index.html).on('complete', function(result) {
+           console.log(result);
+           //outputter(result, program.checks);
+       });
+    }
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
